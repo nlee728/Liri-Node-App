89b97f541039e9896133756c77be9b3644cc8e0a
unknown <38478896+nlee728@users.noreply.github.com>
2018-08-08 23:14:09 -0400

    added twitter logic

--- liri.js
+++ liri.js
@@ -1,25 +1,63 @@
+// Load required files and packages
 require("dotenv").config();
-
-var keys = require("keys.js");
-
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-
+var keys = require("./keys.js");
 var twitter = require("twitter");
-var Spotify = require("node-spotify-api");
+var spotifyAPI = require("node-spotify-api");
 var request = require("request");
+var fs = require('fs');
 
 
-// * `my-tweets`
+//saves user inputs for command into variable
+var command = process.argv[2];
 
-// * `spotify-this-song`
 
-// * `movie-this`
+var log = "./log.txt";
 
-// * `do-what-it-says`
-// What Each Command Should Do
 // node liri.js my-tweets
-// This will show your last 20 tweets and when they were created at in your terminal/bash window.
+// This will show your last 20 tweets and when they were created in your terminal/bash window.
+function getTweets() {
+
+    console.log("Command: " + command);
+    // Append the command to the log file
+	fs.appendFile(log, "User Command: node liri.js my-tweets", function(err) {
+        if (err) throw err;
+        else {
+            console.log("Command logged");
+        }
+    });
+
+    var client = new Twitter(keys.twitter);
+    var params = {screen_name: 'mzlee728', count: 20};
+
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+      if (error) {
+        console.log("Sorry! Could not retrieve tweets.");
+      } else {
+        for (var i = 0; i < tweets.length; i++) {
+                                         "Tweets:\n" + "Created on: " + tweets[i].created_at +
+                                         "\n" + 
+            							 "Tweet content: " + tweets[i].text + "\n" +
+            							 "------------------------\n";
+        }
+
+      }
+    });
+
+
+
+}
+
+// 			// Append the output to the log file
+// 			fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+// 				if (err) throw err;
+// 				console.log(outputStr);
+// 			});
+// 		}
+// 	});
+// }
+
+
+
 // node liri.js spotify-this-song '<song name here>'
 // This will show the following information about the song in your terminal/bash window
 // Artist(s)
@@ -28,6 +66,21 @@ var request = require("request");
 // The album that the song is from
 // If no song is provided then your program will default to "The Sign" by Ace of Base.
 // You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
+function getSong() {
+    console.log("Command: " + command);
+    var song = process.argv[3];
+
+     // Append the command to the log file
+	fs.appendFile("./log.txt", "User Command: node liri.js spotify-this-song\n\n", (err) => {
+        if (err) throw err;
+        else {
+            console.log("Command logged");
+        }
+    });
+
+    var spotify = new Spotify(keys.spotify);
+}
+
 // node liri.js movie-this '<movie name here>'
 // This will output the following information to your terminal/bash window:
 //    * Title of the movie.
@@ -42,13 +95,68 @@ var request = require("request");
 // If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
 // It's on Netflix!
 // You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
+function getMovie() {
+    console.log("Command: " + command);
+    var movie = process.argv[3];
+
+     // Append the command to the log file
+	fs.appendFile("./log.txt", "User Command: node liri.js movie-this\n\n", (err) => {
+        if (err) throw err;
+        else {
+            console.log("Command logged");
+        }
+    });
+}
+
+
 // node liri.js do-what-it-says
-
-
-
-
 // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-
 // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
 // Feel free to change the text in that document to test out the feature for other commands.
+function doIt() {
+    console.log("Command: " + command);
+
+     // Append the command to the log file
+	fs.appendFile("./log.txt", "User Command: node liri.js do-what-it-says\n\n", (err) => {
+        if (err) throw err;
+        else {
+            console.log("Command logged");
+        }
+    });
+    
+}
+
+
+// Run the requested command
+if (command === "my-tweets") {
+	getTweets(); 
+
+} else if (command === "spotify-this-song") {
+	getSong();
+
+} else if (command === "movie-this") {
+	getMovie();
+
+} else if (command ===  "do-what-it-says") {
+	doIt();
+
+} else {
+	// Append the command to the log file
+	fs.appendFile('./log.txt', 'User Command: ' + command + '\n\n', (err) => {
+		if (err) throw err;
+
+		// If the user types in a command that LIRI does not recognize, output the Usage menu 
+		// which lists the available commands.
+		outputStr = 'Usage:\n' + 
+				   '    node liri.js my-tweets\n' + 
+				   '    node liri.js spotify-this-song "<song_name>"\n' + 
+				   '    node liri.js movie-this "<movie_name>"\n' + 
+				   '    node liri.js do-what-it-says\n';
+
+		// Append the output to the log file
+		fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+			if (err) throw err;
+			console.log(outputStr);
+		});
+	});
+}
